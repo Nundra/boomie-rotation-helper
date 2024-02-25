1d05ec5f0a9e1ca17d772663ca140a2ae53373fc
Jeremie <jeremie.cies@gmail.com>
2024-02-25 21:30:44 +0100

    all

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,126 @@
+'use client'
+
+import React, { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import Image from "next/image";
+import { FiChevronsRight } from "react-icons/fi";
+
 
 export default function Home() {
+  const [loading, setLoading] = useState(false)
+  const container = useRef();
+  const { contextSafe } = useGSAP({scope: container});
+  const [rotation, setRotation] = useState(null)
+
+  const handleStartGeneration = contextSafe(() => {
+    setLoading(true)
+    setRotation(null)
+  });
+
+  useEffect(() => {
+    if(loading){
+      handleFakeProgress()
+    }
+  }, [loading])
+
+  const handleFakeProgress = contextSafe(() => {
+    gsap.to(".progress-bar", {
+      width: '100%',
+      duration: 1,
+      onComplete: () => handleGenerateRotation()
+    });
+  });
+
+  const handleGenerateRotation = () => {
+    setLoading(false)
+
+    let rota = ['wrath','wrath','sunfire','moonfire']
+
+    let currentIndex = rota.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [rota[currentIndex], rota[randomIndex]] = [
+        rota[randomIndex], rota[currentIndex]];
+    }
+
+    let part_1 = []
+    part_1.push(rota[0])
+    part_1.push(rota[1])
+    part_1.push('starfire')
+
+    currentIndex = part_1.length;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [part_1[currentIndex], part_1[randomIndex]] = [
+        part_1[randomIndex], part_1[currentIndex]];
+    }
+
+    let part_2 = []
+    part_2.push(rota[2])
+    part_2.push(rota[3])
+    part_2.push('starfire')
+
+    currentIndex = part_2.length;
+
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [part_2[currentIndex], part_2[randomIndex]] = [
+        part_2[randomIndex], part_2[currentIndex]];
+    }
+
+    let finalRota = ['starsurge',...part_1,'starsurge',...part_2]
+    setRotation(finalRota)
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main ref={container} className="px-4 py-8">
+      <h1 className=' text-center text-2xl mb-4'>Boomie Rotation Helper</h1>
+      <div className="flex flex-col justify-center items-center">
+        {!loading && rotation && (
+          <div className="mb-4 flex flex-wrap justify-center items-center gap-2">
+            {rotation.map((spell, index) => (
+              <React.Fragment index={index}>
+                <Image className="rounded border" src={`/${spell}.jpeg`} width={32} height={32} alt={spell} />
+                {index < rotation.length - 1 && (
+                  <FiChevronsRight />
+                )} 
+              </React.Fragment>
+            ))}
+          </div>
+        )}
+        {!loading && (
+          <button onClick={handleStartGeneration} className=" rounded-lg border border-yellow-400 hover:bg-yellow-400 px-4 py-2">Generate my rotation</button>
+        )}
+        {loading && (
+          <div className=' min-w-80'>
+            <p className="text-center mb-2">Charging your new rotation...</p>
+            <div className=" h-1 w-full bg-slate-800 rounded-full">
+              <div className="progress-bar w-0 h-1 bg-yellow-400 rounded-full"></div>
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
